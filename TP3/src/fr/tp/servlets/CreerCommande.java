33c71cce3cb582b62d1e0a4d2358a19b43180965
stagiaire <stagiaire@VM-Ubuntu>
2019-07-05 12:11:55 +0200

    0.3

---
+++ TP3/src/fr/tp/servlets/CreerCommande.java
@@ -0,0 +1,27 @@
+package fr.tp.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CreerCommande extends HttpServlet {
+
+    public static final String VUE = "/WEB-INF/creerCommande.jsp";
+
+    public void doGet( HttpServletRequest request, HttpServletResponse response )
+            throws ServletException, IOException {
+
+        /* Transmission à la page JSP en charge de l'affichage des données */
+        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+    }
+
+    public void doPost( HttpServletRequest request, HttpServletResponse response )
+            throws ServletException, IOException {
+
+        /* Transmission à la page JSP en charge de l'affichage des données */
+        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+    }
+}
